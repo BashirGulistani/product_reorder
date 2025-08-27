131ed1bd6be747a9f93c1209a872897a6b2b7934
Bashir Gulistani <76609477+BashirGulistani@users.noreply.github.com>
2025-08-27 11:38:51 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import requests  # Used to simulate web search
-import google.generativeai as genai
+from google import genai
 
 # --- Page Configuration ---
 st.set_page_config(
